f5ed237196d737f32cf16777ad1a206e215401d4
codebeautyprogramming <codebeautyprogramming@gmail.com>
2024-09-13 00:46:07 +0200

    Startup code for this chapter

---
+++ BankAccountsApp/BankAccount.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccountsApp
+{
+    public class BankAccount
+    {
+        public string Owner { get; set; }
+        public Guid AccountNumber { get; set; }
+        public decimal Balance { get; private set; }
+
+        public BankAccount(string owner)
+        {
+            Owner = owner;
+            AccountNumber = Guid.NewGuid();
+            Balance = 0;
+        }
+
+        public string Deposit(decimal amount)
+        {
+            if (amount <= 0)
+                return "You can not deposit $"+amount;
+            if (amount > 20000)
+                return "AML Deposit Limit Reached.";
+
+            Balance += amount;
+            return "Deposit completed successfully.";
+        }
+
+        public string Withdraw(decimal amount)
+        {
+            if (amount <= 0)
+                return "You can not withdraw $" + amount;
+            if (amount > Balance)
+                return "You don't have enough money.";
+
+            Balance -= amount;
+            return "Withdraw completed successfully.";
+        }
+
+
+    }
+}
---
+++ BankAccountsApp/Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace BankAccountsApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            OwnerTxt = new TextBox();
+            AmountNum = new NumericUpDown();
+            BankAccountsGrid = new DataGridView();
+            DepositBtn = new Button();
+            WithdrawBtn = new Button();
+            CreateAccountBtn = new Button();
+            label3 = new Label();
+            InterestRateNum = new NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)AmountNum).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)BankAccountsGrid).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)InterestRateNum).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(36, 35);
+            label1.Margin = new Padding(5, 0, 5, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(79, 30);
+            label1.TabIndex = 0;
+            label1.Text = "Owner:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(36, 276);
+            label2.Margin = new Padding(5, 0, 5, 0);
+            label2.Name = "label2";
+            label2.Size = new Size(93, 30);
+            label2.TabIndex = 1;
+            label2.Text = "Amount:";
+            // 
+            // OwnerTxt
+            // 
+            OwnerTxt.Location = new Point(123, 35);
+            OwnerTxt.Name = "OwnerTxt";
+            OwnerTxt.Size = new Size(229, 35);
+            OwnerTxt.TabIndex = 2;
+            // 
+            // AmountNum
+            // 
+            AmountNum.Location = new Point(137, 271);
+            AmountNum.Maximum = new decimal(new int[] { 9999999, 0, 0, 0 });
+            AmountNum.Minimum = new decimal(new int[] { 9999999, 0, 0, int.MinValue });
+            AmountNum.Name = "AmountNum";
+            AmountNum.Size = new Size(215, 35);
+            AmountNum.TabIndex = 3;
+            // 
+            // BankAccountsGrid
+            // 
+            BankAccountsGrid.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            BankAccountsGrid.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            BankAccountsGrid.Location = new Point(411, 35);
+            BankAccountsGrid.Name = "BankAccountsGrid";
+            BankAccountsGrid.Size = new Size(503, 219);
+            BankAccountsGrid.TabIndex = 4;
+            // 
+            // DepositBtn
+            // 
+            DepositBtn.Location = new Point(411, 264);
+            DepositBtn.Name = "DepositBtn";
+            DepositBtn.Size = new Size(247, 54);
+            DepositBtn.TabIndex = 5;
+            DepositBtn.Text = "Deposit";
+            DepositBtn.UseVisualStyleBackColor = true;
+            DepositBtn.Click += DepositBtn_Click;
+            // 
+            // WithdrawBtn
+            // 
+            WithdrawBtn.Location = new Point(664, 264);
+            WithdrawBtn.Name = "WithdrawBtn";
+            WithdrawBtn.Size = new Size(250, 54);
+            WithdrawBtn.TabIndex = 6;
+            WithdrawBtn.Text = "Withdraw";
+            WithdrawBtn.UseVisualStyleBackColor = true;
+            WithdrawBtn.Click += WithdrawBtn_Click;
+            // 
+            // CreateAccountBtn
+            // 
+            CreateAccountBtn.Location = new Point(123, 137);
+            CreateAccountBtn.Name = "CreateAccountBtn";
+            CreateAccountBtn.Size = new Size(229, 54);
+            CreateAccountBtn.TabIndex = 7;
+            CreateAccountBtn.Text = "Create account";
+            CreateAccountBtn.UseVisualStyleBackColor = true;
+            CreateAccountBtn.Click += CreateAccountBtn_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(36, 89);
+            label3.Name = "label3";
+            label3.Size = new Size(165, 30);
+            label3.TabIndex = 8;
+            label3.Text = "Interest rate (%):";
+            // 
+            // InterestRateNum
+            // 
+            InterestRateNum.Location = new Point(214, 87);
+            InterestRateNum.Name = "InterestRateNum";
+            InterestRateNum.Size = new Size(138, 35);
+            InterestRateNum.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(12F, 30F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(934, 330);
+            Controls.Add(InterestRateNum);
+            Controls.Add(label3);
+            Controls.Add(CreateAccountBtn);
+            Controls.Add(WithdrawBtn);
+            Controls.Add(DepositBtn);
+            Controls.Add(BankAccountsGrid);
+            Controls.Add(AmountNum);
+            Controls.Add(OwnerTxt);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 238);
+            Margin = new Padding(5, 6, 5, 6);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)AmountNum).EndInit();
+            ((System.ComponentModel.ISupportInitialize)BankAccountsGrid).EndInit();
+            ((System.ComponentModel.ISupportInitialize)InterestRateNum).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private TextBox OwnerTxt;
+        private NumericUpDown AmountNum;
+        private DataGridView BankAccountsGrid;
+        private Button DepositBtn;
+        private Button WithdrawBtn;
+        private Button CreateAccountBtn;
+        private Label label3;
+        private NumericUpDown InterestRateNum;
+    }
+}
